ccf531b3e21692ba7442ab7f85886d07155b6667
Max Holzleitner <max.holzleitner@instant-it.at>
2017-06-25 00:21:20 +0200

    implemented starting the navigation activity

--- controller/src/main/java/at/tripwire/vbeltcontroller/MainActivity.java
+++ controller/src/main/java/at/tripwire/vbeltcontroller/MainActivity.java
@@ -1,11 +1,34 @@
 package at.tripwire.vbeltcontroller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    @ViewById(R.id.start)
+    protected EditText startEditText;
 
+    @ViewById(R.id.end)
+    protected EditText endEditText;
+
+    @Click(R.id.navigate)
+    protected void navigateClicked() {
+        String start = startEditText.getText().toString();
+        String end = endEditText.getText().toString();
+        if (!start.isEmpty() && !end.isEmpty()) {
+            Intent intent = new Intent(this, NavigationActivity_.class);
+            intent.putExtra(NavigationActivity.EXTRA_START, start);
+            intent.putExtra(NavigationActivity.EXTRA_END, end);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.fill_out_fields, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
