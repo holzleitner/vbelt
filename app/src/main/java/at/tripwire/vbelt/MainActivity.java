b0caae5c35dd6cd36aaf10207920cb8c2cc4b51f
Max Holzleitner <max.holzleitner@instant-it.at>
2017-06-24 16:05:45 +0200

    added a method to handle a click on the connect button

--- app/src/main/java/at/tripwire/vbelt/MainActivity.java
+++ app/src/main/java/at/tripwire/vbelt/MainActivity.java
@@ -2,9 +2,14 @@ package at.tripwire.vbelt;
 
 import android.support.v7.app.AppCompatActivity;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    @Click(R.id.connect_button)
+    protected void connectClicked() {
+        // start service
+    }
 }
