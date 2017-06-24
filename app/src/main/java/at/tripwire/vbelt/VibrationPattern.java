b5368095dc1725e9d7ac9a5cabe059268e8b32b8
Max Holzleitner <max.holzleitner@instant-it.at>
2017-06-24 19:02:20 +0200

    refactored the vibration patterns

--- app/src/main/java/at/tripwire/vbelt/VibrationPattern.java
+++ app/src/main/java/at/tripwire/vbelt/VibrationPattern.java
@@ -2,10 +2,10 @@ package at.tripwire.vbelt;
 
 public enum VibrationPattern {
 
-    DISTANCE_100M(100, new long[]{0, 300, 200, 300, 200, 300}),
-    DISTANCE_10M(10, new long[]{0, 300, 200, 300}),
-    DISTANCE_1M(1, new long[]{0, 300}),
-    DISTANCE_IMMEDIATELY(0, new long[]{0, 1000});
+    DISTANCE_100M(100, new long[]{0, 300}),
+    DISTANCE_50M(50, new long[]{0, 300, 200, 300}),
+    DISTANCE_10M(10, new long[]{0, 300, 200, 300, 200, 300}),
+    DISTANCE_1M(1, new long[]{0, 1000});
 
     private int distance;
 
