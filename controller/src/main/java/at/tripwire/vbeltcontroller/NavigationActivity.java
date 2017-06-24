594faa98c509100daaa3fc204f1001a542fef9ef
Max Holzleitner <max.holzleitner@instant-it.at>
2017-06-25 01:18:23 +0200

    implemented the navigation activity user interface

--- controller/src/main/java/at/tripwire/vbeltcontroller/NavigationActivity.java
+++ controller/src/main/java/at/tripwire/vbeltcontroller/NavigationActivity.java
@@ -1,8 +1,11 @@
 package at.tripwire.vbeltcontroller;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_navigation)
 public class NavigationActivity extends AppCompatActivity {
@@ -11,4 +14,11 @@ public class NavigationActivity extends AppCompatActivity {
 
     public static final String EXTRA_END = "at.tripwire.vbeltcontroller.extra.end";
 
+    @ViewById(R.id.location)
+    protected TextView locationTextView;
+
+    @AfterViews
+    protected void init() {
+
+    }
 }
