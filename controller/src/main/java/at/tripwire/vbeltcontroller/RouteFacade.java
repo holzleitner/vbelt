e4ea981e5d02e0a21c13db9582f9b94286e0cff8
Max Holzleitner <max.holzleitner@instant-it.at>
2017-06-25 21:52:32 +0200

    removed using the google-maps-services

--- controller/src/main/java/at/tripwire/vbeltcontroller/RouteFacade.java
+++ controller/src/main/java/at/tripwire/vbeltcontroller/RouteFacade.java
@@ -4,11 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
-import com.google.maps.GeoApiContext;
-import com.google.maps.RoadsApi;
-import com.google.maps.model.SnappedPoint;
 
-import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.RootContext;
 import org.androidannotations.rest.spring.annotations.RestService;
@@ -22,23 +18,11 @@ import java.util.List;
 @EBean(scope = EBean.Scope.Singleton)
 public class RouteFacade {
 
-    private static final int PAGE_SIZE_LIMIT = 100;
-
-    private static final int PAGINATION_OVERLAP = 5;
-
     @RestService
     protected RouteClient routeClient;
 
     @RootContext
     protected Context context;
-
-    private GeoApiContext geoApiContext;
-
-    @AfterInject
-    protected void init() {
-        geoApiContext = new GeoApiContext();
-        geoApiContext.setApiKey(context.getString(R.string.google_maps_web_services_key));
-    }
 
     public List<LatLng> getPoints(String srcLat, String srcLon, String destLat, String destLon) {
         String content = routeClient.getPoints(srcLat, srcLon, destLat, destLon);
@@ -65,7 +49,7 @@ public class RouteFacade {
                     }
                 }
             }
-            return snapToRoads(geoPoints);
+            return geoPoints;
         } catch (JSONException e) {
             Log.e(context.getString(R.string.app_name), "Error loading route points.", e);
         }
@@ -101,43 +85,5 @@ public class RouteFacade {
             poly.add(p);
         }
         return poly;
-    }
-
-    private List<LatLng> snapToRoads(List<LatLng> geoPoints) {
-        List<LatLng> snappedPoints = new ArrayList<>();
-
-        int offset = 0;
-        while (offset < geoPoints.size()) {
-            if (offset > 0) {
-                offset -= PAGINATION_OVERLAP;
-            }
-            int lowerBound = offset;
-            int upperBound = Math.min(offset + PAGE_SIZE_LIMIT, geoPoints.size());
-
-            List<LatLng> subList = geoPoints.subList(lowerBound, upperBound);
-            com.google.maps.model.LatLng[] page = new com.google.maps.model.LatLng[upperBound - lowerBound];
-            for (int i = 0; i < subList.size(); i++) {
-                LatLng latLng = subList.get(i);
-                page[i] = new com.google.maps.model.LatLng(latLng.latitude, latLng.longitude);
-            }
-
-            SnappedPoint[] points = new SnappedPoint[0];
-            try {
-                points = RoadsApi.snapToRoads(geoApiContext, page).await();
-            } catch (Exception e) {
-                Log.e(context.getString(R.string.app_name), "Failed to use the road api.", e);
-            }
-            boolean passedOverlap = false;
-            for (SnappedPoint point : points) {
-                if (offset == 0 || point.originalIndex >= PAGINATION_OVERLAP - 1) {
-                    passedOverlap = true;
-                }
-                if (passedOverlap) {
-                    snappedPoints.add(new LatLng(point.location.lat, point.location.lng));
-                }
-            }
-            offset = upperBound;
-        }
-        return snappedPoints;
     }
 }
