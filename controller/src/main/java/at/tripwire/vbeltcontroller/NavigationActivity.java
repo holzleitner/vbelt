0b38c9fc8ce2211a7bef3ab92b87b4fd98e5110a
Max Holzleitner <max.holzleitner@instant-it.at>
2017-06-25 14:29:17 +0200

    implemented receiving the current location

--- controller/src/main/java/at/tripwire/vbeltcontroller/NavigationActivity.java
+++ controller/src/main/java/at/tripwire/vbeltcontroller/NavigationActivity.java
@@ -1,10 +1,19 @@
 package at.tripwire.vbeltcontroller;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_navigation)
@@ -17,8 +26,43 @@ public class NavigationActivity extends AppCompatActivity {
     @ViewById(R.id.location)
     protected TextView locationTextView;
 
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     @AfterViews
     protected void init() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // start location updates
+        try {
+            fusedLocationProviderClient.requestLocationUpdates(LocationRequest.create(), locationCallback, null);
+        } catch (SecurityException e) {
+            Log.e(getString(R.string.app_name), "Failed to request the current location.", e);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // stop location updates
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private LocationCallback locationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location currentLocation = locationResult.getLastLocation();
+
+            updateUI(currentLocation);
+        }
+    };
+
+    @UiThread
+    private void updateUI(Location location) {
+        locationTextView.setText(location.toString());
     }
 }
