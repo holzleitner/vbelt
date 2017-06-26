3bdccd9491992f9a302a0c9260a67211bac1d6ec
Max Holzleitner <max.holzleitner@instant-it.at>
2017-06-26 15:28:20 +0200

    added a demo activity

---
+++ controller/src/main/java/at/tripwire/vbeltcontroller/DemoActivity.java
@@ -0,0 +1,10 @@
+package at.tripwire.vbeltcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_demo)
+public class DemoActivity extends AppCompatActivity {
+
+}
